14ab3264b98211d93d43dfda182247d188803b20
rnacharjee <rajib.ipe@gmail.com>
2021-10-21 17:38:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,113 @@
+const timer = document.getElementById("timer");
+var hr = 0 ;
+var min = 0;
+var sec = 0;
+var stopTime = true ;
+var laps =[];
+var lapSecond = [0];
+var header = ["Split Time" , "Cycle Time (Sec)"];
+let myTable = document.querySelector('#table');
+
+function startTimer(){
+    if(stopTime == true){
+        stopTime = false;
+        console.log(" start button clicked");
+        timeCycle()
+        console.log(" start button clicked");
+    }
+
+}
+function stopTimer(){
+    if(stopTime == false){
+        stopTime = true;
+    }
+}
+function timeCycle(){
+    
+    if(stopTime == false){
+        sec = parseInt(sec);
+        min = parseInt(min);
+        hr = parseInt(hr);
+        sec = sec + 1;
+
+        if(sec == 60){
+            min = min +1;
+            sec=0;
+        }
+        if(min == 60){
+            hr = hr+1;
+            min = 0;
+        }
+
+        if(sec < 10 || sec == 0){
+            sec = '0'+ sec;
+        }
+        if (min < 10 || min == 0) {
+            min = '0' + min;
+        }
+        if (hr < 10 || hr == 0) {
+            hr = '0' + hr;
+        }
+        timer.innerHTML = hr + ':' + min + ':' + sec;
+        
+        setTimeout("timeCycle()", 1000);
+
+    }
+}
+function resetTimer() {
+    timer.innerHTML = "00:00:00";
+    stopTime = true;
+    hr = 0;
+    sec = 0;
+    min = 0;
+    laps=[];
+    lapSecond=[0];
+    document.getElementById("table").innerHTML="";
+}
+
+function lapTimer(){
+    
+    
+    let time = document.getElementById("timer").innerText
+    document.getElementById("table").innerHTML="";
+    let timeSeparator = time.split(":")
+    let totalSecond = parseInt(timeSeparator[0])*3600 + parseInt(timeSeparator[1])*60 + parseInt(timeSeparator[2])
+    lapSecond.push(totalSecond);
+
+    countedSecond = lapSecond[lapSecond.length-1]-lapSecond[lapSecond.length-2]
+
+    laps.push([document.getElementById("timer").innerText,countedSecond]);
+
+
+    
+    document.getElementById("lap-time").innerHTML = "";
+    let list = document.getElementById("lap-time");
+    
+    console.log(countedSecond)
+
+    let table = document.createElement('table');
+    let headerRow = document.createElement('tr');
+
+    header.forEach(headerText => {
+        let header = document.createElement("th")
+        let textNode = document.createTextNode(headerText)
+        header.appendChild(textNode)
+        headerRow.appendChild(header)
+    });
+    table.appendChild(headerRow)
+
+    laps.forEach(lap =>{
+        let row = document.createElement("tr")
+        Object.values(lap).forEach(text =>{
+            let cell = document.createElement("td")
+            let textNode= document.createTextNode(text)
+            cell.appendChild(textNode)
+            row.appendChild(cell)
+        })
+        table.appendChild(row)
+    })
+
+    myTable.appendChild(table);
+
+    
+}
